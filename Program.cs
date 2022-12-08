162b36fbf89af1d00b303611ab4214dcb57a4f1b
Roman Shuhevich <popados2005@gmail.com>
2022-12-08 11:34:56 +0200

    Додано нові зміни

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp11
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye world");
+            Console.WriteLine("lyublyu bnani");
         }
     }
 }
